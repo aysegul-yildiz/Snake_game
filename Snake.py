626caad53c2aa296a2ef294d473dda945602c33c
Ayşegül Yıldız <99360744+aysegul-yildiz@users.noreply.github.com>
2024-01-16 00:44:08 +0300

    Update Snake.py
    snake class is also updated so the game can start over after the player has lost

--- Snake.py
+++ Snake.py
@@ -33,6 +33,13 @@ class Snake:
             self.snake[snake_num].goto(new_x,new_y)
         self.head.forward(DISTANCE)
 
+    def reset(self):
+        for s in self.snake:
+            s.goto(1000,1000)
+        self.snake.clear()
+        self.createSnake()
+        self.head = self.snake[0]
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
